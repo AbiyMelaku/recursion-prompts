b9c35055fd2632e1849ecc3dea729bb8de2b4dc1
Abiy Melaku <agirma08@gmail.com>
2017-12-03 22:36:20 -0800

    progress so far

--- src/recursion.js
+++ src/recursion.js
@@ -159,6 +159,26 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+  //'base cases'-->
+  if( y === 0 ){ return NaN; }
+
+  if( x < 0 && y < 0 ){
+    if( x > y ){ 
+      return x; 
+    }
+  } else if( (x < 0 && y > 0) || (x > 0 && y < 0) ){
+    if( -x < y ){ 
+      return x; 
+    }
+  //recursive case 1' --> 
+    return modulo(x + y, y);
+  } else {
+    if( x < y ){ 
+      return x; 
+    }
+  }
+  //'recursive case 2'-->
+  return modulo(x - y, y);
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
